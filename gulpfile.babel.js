df629f323504570d82bbf3c1006cef949fffd861
gbwlxhd97 <gbwlxhd97@naver.com>
2021-07-05 00:31:16 +0900

    gulp-webserver -watch mod

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from  "del";
+import ws from "gulp-webserver";
 
 const routes = {
     pug: {
+        watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
     }
@@ -17,8 +19,17 @@ const pug = () => gulp
 
 const clean = () => del(["build"]); // 빌드실수를 했을때 삭제하고 다시 만들어주는 모듈
 
+const webserver = () => gulp.src("build").pipe(ws({livereload: true, open: true}));
+
+const watch = () => { //-w , 왓치모드 컴파일 바로바로해주는기능
+    gulp.watch(routes.pug.watch, pug);
+}
+
+
 const prepare = gulp.series([clean]);
 
-const assets = gulp.series([pug])
+const assets = gulp.series([pug]);
 
-export const dev =  gulp.series([prepare,assets]); 
+const postDev = gulp.series([webserver,watch]); //동시에 두가지 Task를 처리하는 방법 그냥 저렇게 일렬로 쓰고 , 로 구분해주기
+
+export const dev =  gulp.series([prepare,assets,postDev]); 
