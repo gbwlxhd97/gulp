511eac148ce82626d5ad4917a032d2ac3b9030f4
gbwlxhd97 <gbwlxhd97@naver.com>
2021-07-05 15:23:34 +0900

    Gulp Sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import gpug from "gulp-pug";
 import del from  "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
+//import sass from "gulp-sass";
+
+//sass.compiler = require("sass");
 
 const routes = {
     pug: {
@@ -13,7 +16,12 @@ const routes = {
     img: {
         src: "src/img/*",
         dest: "build/img"
-    }
+    },
+    // scss: {
+    //     watch: "src/scss/**/*.scss",
+    //     src: "src/scss/style.css",
+    //     dest: "build/css"
+    // }
 };
 
 
@@ -28,6 +36,7 @@ const webserver = () => gulp.src("build").pipe(ws({livereload: true, open: true}
 
 const watch = () => { //-w , 왓치모드 컴파일 바로바로해주는기능
     gulp.watch(routes.pug.watch, pug);
+    //gulp.watch(routes.scss.watch, styles);
 }
 
 const img = () => gulp
@@ -35,9 +44,15 @@ const img = () => gulp
     .pipe(image())
     .pipe(gulp.dest(routes.img.dest));
 
+
+// const styles = () =>  gulp
+//     .src(routes.scss)
+//     .pipe(sass().on("error", sass.logError))
+//     .pipe(gulp.dest(routes.scss.dest));   
+
 const prepare = gulp.series([clean,img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug,/*styles*/]);
 
 const postDev = gulp.series([webserver,watch]); //동시에 두가지 Task를 처리하는 방법 그냥 저렇게 일렬로 쓰고 , 로 구분해주기
 
